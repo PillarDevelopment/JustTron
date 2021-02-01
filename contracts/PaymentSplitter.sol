7841460d80838557936459810796cf014d0c6d82
i9616271555 <i9616271555@gmail.com>
2021-02-01 11:29:28 +0300

    reorganized

--- contracts/PaymentSplitter.sol
+++ contracts/PaymentSplitter.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.4;
+pragma solidity ^0.5.8;
 
 contract Context {
     // Empty internal constructor, to prevent people from mistakenly deploying
--- tronbox.js
+++ tronbox.js
@@ -44,7 +44,7 @@ Then, run the migration with:
     },
     compilers: {
       solc: {
-        version: '0.5.4'
+        version: '0.5.8'
       }
     }
   }
